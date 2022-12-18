41bc7aa8620b030b80e98779d4c9f47d0eebe696
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-12-18 12:39:03 +0700

    feat: создать роуты

---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={ <Main /> } />
+        <Route path='/movies' element={ <Movies /> } />
+        <Route path='/saved-movies' element={ <SavedMovies /> } />
+        <Route path='/profile' element={ <Profile /> } />
+        <Route path='/signin' element={ <Login /> } />
+        <Route path='/signup' element={ <Register /> } />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
